449382a3e64f60662150e45a8623a20008c575ba
Kieren Anderson <kjander0@gmail.com>
2022-07-21 23:37:35 +1000

    Preparing for first bit of codegen

--- codegen.py
+++ codegen.py
@@ -1,1 +1,11 @@
-sdada
+template = '''
+int main() {
+    <<!statements>>
+}
+'''
+
+def gen(program):
+    c_statements = [gen_statement(s) for s in program]
+    return template.replace('<<!statements>>', '\n'.join(c_statements))
+    
+    
